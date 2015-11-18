1655ec9d8e6eddd92dc98e4e56810a5334c613b9
Amine <amineatallah@gmail.com>
2015-11-18 15:31:03 +0200

    add demo examples

--- demo/demoApp.js
+++ demo/demoApp.js
@@ -2,7 +2,7 @@
   angular.module('sharingApp', ['angularSocializer'])
   .config(function(socializerConfigProvider){
     socializerConfigProvider.setFacebookAppId('947260665329944');
-    socializerConfigProvider.setTwitterAccount('amineatallah');
+    socializerConfigProvider.setTwitterAccount('globalName');
   })
   .controller('test', function(socializerConfig){
   });
