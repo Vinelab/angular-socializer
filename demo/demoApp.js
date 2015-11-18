101546477a7d992d87aa0157e3fa203daa56d7dd
Amine <amineatallah@gmail.com>
2015-11-18 17:16:23 +0200

    demo code

--- demo/demoApp.js
+++ demo/demoApp.js
@@ -4,6 +4,10 @@
     socializerConfigProvider.setFacebookAppId('947260665329944');
     socializerConfigProvider.setTwitterAccount('globalName');
   })
-  .controller('test', function(socializerConfig){
+  .controller('test', function(facebookCount){
+    facebookCount.getFacebookCount('http://google.com')
+      .then(function(response) {
+        console.log(response);
+      });
   });
 })();
